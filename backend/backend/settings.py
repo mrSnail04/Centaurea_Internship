90688a24b23bd10cf29085f129c8da8de87b2f20
Maksim Bulauski <maks.bulavsky@gmail.com>
2021-11-29 13:01:22 +0300

    upd

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,6 +139,11 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+db_from_env = dj_database_url.config(
+    default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+DATABASES['default'].update(db_from_env)
 #
 # DATABASES = {
 #     'default': {
