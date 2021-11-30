6cca43828cee24730eaffea1f635b45b14a36d5d
Maksim Bulauski <maks.bulavsky@gmail.com>
2021-11-30 14:41:52 +0300

    upd

--- backend/scripts/entrypoint-dev.sh
+++ backend/scripts/entrypoint-dev.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
-
 python manage.py runserver 0.0.0.0:$PORT
--- backend/scripts/entrypoint-prod.sh
+++ backend/scripts/entrypoint-prod.sh
@@ -2,5 +2,4 @@
 
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
-
 python backend/manage.py runserver 0.0.0.0:$PORT
