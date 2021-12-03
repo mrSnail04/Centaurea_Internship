a845aacef5808af5e372e5c5ea92b10468b5e5a1
Maksim Bulauski <maks.bulavsky@gmail.com>
2021-12-03 14:48:04 +0300

    upd

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -65,6 +65,7 @@ CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
+    '*',
     'http://localhost:3000',
     'https://ancient-oasis-20487.herokuapp.com',
 ]
