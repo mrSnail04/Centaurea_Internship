c2e9b238a549ed6f8221f24b6fb1ed15344a2161
Maksim Bulauski <maks.bulavsky@gmail.com>
2021-12-03 20:49:44 +0300

    upd

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 urlpatterns = [
     path('', include('concert.urls')),
     path('admin/', admin.site.urls),
-    path('api', include('shop_api.main.router')),
+    path('api/', include('shop_api.main.router')),
     path('accounts/', include('allauth.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
