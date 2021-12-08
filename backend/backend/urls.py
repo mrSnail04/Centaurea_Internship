9dc3192329b2012358bc474879435c0f8e90f1e8
Maksim Bulauski <maks.bulavsky@gmail.com>
2021-12-08 18:10:44 +0300

    upd

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -11,11 +11,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
-    re_path(r'^static/(?P<path>.*)$', serve,
-            {'document_root': settings.STATIC_ROOT}),
-    re_path(r'^media/(?P<path>.*)$', serve, {
-        "document_root": settings.MEDIA_ROOT
-})
 ]
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
