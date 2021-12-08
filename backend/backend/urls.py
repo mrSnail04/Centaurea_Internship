5e1d63ec2aa92fe3175af41c8f7f78d9a5050890
Maksim Bulauski <maks.bulavsky@gmail.com>
2021-12-08 17:40:47 +0300

    upd

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-
+from django.urls import re_path
 
 urlpatterns = [
     path('', include('concert.urls')),
