8c45776378f216d7a74b47cd2bcfd67fd527e040
Maksim Bulauski <maks.bulavsky@gmail.com>
2021-12-16 20:18:45 +0300

    upd

--- frontend/src/containers/EventPage.js
+++ frontend/src/containers/EventPage.js
@@ -11,8 +11,8 @@ export const EventPage = (props) => {
     let {slug} = useParams(); //{slug: 'halloween'}
 
     const increase = async (count) => {
-        let newcount = count + 1
-        console.log(count)
+        let newcount = {count} + 1
+        console.log({count})
         console.log(newcount)
         setCount(newcount);
     }
